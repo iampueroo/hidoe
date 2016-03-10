bb41a5e8d04913f068325735acbedb15c3433fea
mswarzenski <marie.swarzenski@gmail.com>
2016-03-09 20:40:23 -0800

    2013-14 basic

--- CreateCSV.R
+++ CreateCSV.R
@@ -17,7 +17,7 @@ CreateCSV <- function() {
   setwd("~/dropbox/rh1/hidoe/input-csv/classroom/13.07.20 - 14.10.20") 
   cr <- do.call(rbind,lapply(dir(), read.csv)) #read all classroom sensor data
   names(cr) <- c("RoomID", "SensorAlias", "StartTime", "Temp_F", "RH", "Ill")
-  cr.temp <- cr[!is.na(cr$Temp_F),c("RoomID", "StartTime", "Temp_F")] #pull out temp
+  cr.temp <- cr[!is.na(cr$Temp_F),c("RoomID", "StartTime", "Temp_F", "SensorAlias")] #pull out temp
   cr.rh<- cr[!is.na(cr$RH),c("RoomID", "StartTime", "RH")] #pull out RH
   cr <- merge(cr.temp, cr.rh, by=c("RoomID", "StartTime")) #merge back together to compress data frame
   cr$StartTime <- as.character(cr$StartTime)
@@ -26,7 +26,7 @@ CreateCSV <- function() {
   cr$Date <- as.Date(cr$StartTime, format="%m/%d/%y")  
   cr$Time <- format(strptime(cr$StartTime, format="%m/%d/%y %H:%M"), format="%H:%M") 
   cr$Month <- factor(format(cr$Date, "%B"), c("August", "September", "October", "November", "December", "January", "February", "March", "April", "May"))
-  cr <- cr[,c("RoomID", "Temp_F", "UTCI_F", "Date", "Time", "Month")]
+  cr <- cr[,c("RoomID", "Temp_F", "UTCI_F", "Date", "Time", "Month", "SensorAlias")]
   cr <- merge(cr, sdates, by="Date", all.x=FALSE) #restrict to school days
   cr <- merge(cr, shours, by="Time", all.x=FALSE) #restrict to school hours 
   
